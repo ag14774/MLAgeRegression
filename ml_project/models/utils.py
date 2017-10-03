76cb3b7da6fc8f6007d819c5aa93df571bd707f6
Andreas Georgiou <a.georgiou.2014@bristol.ac.uk>
2017-10-03 20:45:53 +0200

    Flatten, ShadeExtraction, StandardScaler, VarianceThreshold, LinearRegression

---
+++ ml_project/models/utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+import sys
+
+
+class Debugger(BaseEstimator, TransformerMixin):
+    """Debugger"""
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        print(X.shape)
+        sys.stdout.flush()
+        return X
