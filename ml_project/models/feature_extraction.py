5cade6ffe563fa33139b1d229f014aff2d2f612e
Andreas Georgiou <a.georgiou.2014@bristol.ac.uk>
2017-10-04 12:34:28 +0200

    Variance-Extraction with Kmeans-Scaler-Variance-Linear

--- ml_project/models/feature_extraction.py
+++ ml_project/models/feature_extraction.py
@@ -8,19 +8,22 @@ from sklearn.utils.random import sample_without_replacement
 from sklearn.utils.validation import check_array, check_is_fitted
 
 
-def runKmeans(Xrow, args):
+def calcBinBoundaries(Xrow, args):
     Xrow = np.reshape(Xrow, (-1, 1))
-    Xrow = args["mbk"].fit_predict(Xrow)
-    print(Xrow, Xrow.shape)
+    args["mbk"].fit(Xrow)
+    print("Sample center", args["mbk"].cluster_centers_)
     sys.stdout.flush()
-    return np.bincount(Xrow, minlength=args["minlength"])
+    return np.sort(args["mbk"].cluster_centers_)
 
 
 class ShadeExtraction(BaseEstimator, TransformerMixin):
     """Count how many pixels fall in each shade category"""
 
-    def __init__(self, n_shades=10):
+    def __init__(self, n_shades=10, n_rows=5, density=False):
         self.n_shades = n_shades
+        self.n_rows = n_rows
+        self.density = density
+        self.boundaries = None
 
 
 #NEXT STEPS: USE KMEANS INSTEAD OF GAUSSIAN
@@ -28,18 +31,37 @@ class ShadeExtraction(BaseEstimator, TransformerMixin):
 #ALSO TRY SPREAD MATRIX WITH LOWER EPS
 
     def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
         X = check_array(X)
+        n_samples, n_features = X.shape
         mbk = MiniBatchKMeans(
             n_clusters=self.n_shades,
             batch_size=1000,
             n_init=5,
             reassignment_ratio=0.02)
-        X = np.apply_along_axis(runKmeans, 1, X,
-                                {"mbk": mbk,
-                                 "minlength": self.n_shades})
+        sample_rows = sample_without_replacement(n_features, self.n_rows)
+        X_subset = X[sample_rows]
+        X_subset = np.apply_along_axis(calcBinBoundaries, 1, X_subset,
+                                       {"mbk": mbk})
+        averaged_centers = np.mean(X_subset, axis=0)
+        print("Averaged centers", averaged_centers)
+        sys.stdout.flush()
+        self.boundaries = np.zeros(self.n_shades + 1)
+        for i in range(1, self.n_shades):
+            self.boundaries[i] = (
+                averaged_centers[i - 1] + averaged_centers[i]) / 2
+        self.boundaries[self.n_shades] = 99999999999
+        self.boundaries = np.round(self.boundaries)
+        print("Final boundaries: ", self.boundaries)
+        sys.stdout.flush()
+        return self
+
+    def transform(self, X, y=None):
+        check_is_fitted(self, ["boundaries"])
+        print("Shape before bin count: ", X.shape)
+        sys.stdout.flush()
+        X = check_array(X)
+        X = np.apply_along_axis(
+            np.histogram, 1, X, bins=self.boundaries, density=self.density)
         print("X after bincount: ")
         print(X)
         sys.stdout.flush()
