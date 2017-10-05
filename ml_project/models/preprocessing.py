edb00cd44ee54dfde0e19d9e92fca764d6fe3591
Andreas Georgiou <a.georgiou.2014@bristol.ac.uk>
2017-10-05 02:02:22 +0200

    Created SampleScaler

--- ml_project/models/preprocessing.py
+++ ml_project/models/preprocessing.py
@@ -54,20 +54,19 @@ class Flatten(BaseEstimator, TransformerMixin):
         return X.reshape(X.shape[0], -1)
 
 
-class StandardScalerTranspose(BaseEstimator, TransformerMixin):
+class ScaleEachSample(BaseEstimator, TransformerMixin):
     def __init__(self, enabled=True, with_std=True):
         self.enabled = enabled
         self.with_std = with_std
-        self.scaler = None
 
     def fit(self, X, y=None):
-        if (self.enabled):
-            self.scaler = StandardScaler(copy=False, with_std=self.with_std)
-            self.scaler.fit(X)
         return self
 
     def transform(self, X, y=None):
         if (self.enabled):
-            X = check_array(X)
-            X = self.scaler.transform(X)
+            means = np.mean(X, axis=1)
+            stds = np.std(X, axis=1)
+            X = X - means
+            if(self.with_std is True):
+                X = X / stds
         return X
